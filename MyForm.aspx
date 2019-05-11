739f599c31c73472de4f8e59e4d799afc1e54d66
Aravind Raj <aravinddevaraj777@gmail.com>
2019-05-11 15:31:33 +0530

    Initial commit

---
+++ MyForm.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Async="true" Language="C#" AutoEventWireup="true" CodeFile="MyForm.aspx.cs" Inherits="MyForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+           <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="click" OnClick="Button1_Click" />
+        </p>
+        
+        <asp:Label ID="Label2" runat="server" Text="Value1"></asp:Label>&nbsp;&nbsp;  <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Value2"></asp:Label>&nbsp;&nbsp;   <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox><br /><br />
+        <asp:Label ID="Label4" runat="server" Text="Result"></asp:Label> &nbsp;&nbsp;  <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox><br /><br />
+        <asp:Button ID="Button2" runat="server" Text="Add" OnClick="Button2_Click" />
+
+
+    </form>
+</body>
+</html>
---
+++ MyForm.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net; //for WebClient
+using System.Xml.Linq; //for XDocument
+
+public partial class MyForm : System.Web.UI.Page
+{
+    // object to invoke the WelcomeRESTXMLService
+    private WebClient client = new WebClient();
+    private WebClient client1 = new WebClient();
+
+
+    private XNamespace xmlNamespace = XNamespace.Get("http://schemas.microsoft.com/2003/10/Serialization/");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // add DownloadStringCompleted event handler to WebClient
+        client.DownloadStringCompleted +=
+        new DownloadStringCompletedEventHandler(client_DownloadStringCompleted);
+        client1.DownloadStringCompleted +=
+        new DownloadStringCompletedEventHandler(client1_DownloadStringCompleted);
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        // send request to WelcomeRESTXMLService
+        client.DownloadStringAsync(new Uri("http://localhost:1096/Service.svc/welcome/" + TextBox1.Text));
+    }
+    // process web service response
+    private void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+    {
+        // check if any error occurred in retrieving service data
+        if (e.Error == null)
+        {
+            
+            // parse the returned XML string (e.Result)
+            XDocument xmlResponse = XDocument.Parse(e.Result);
+            Label1.Text = xmlResponse.Element(xmlNamespace + "string").Value;
+        }
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        client1.DownloadStringAsync(new Uri("http://localhost:1096/Service.svc/add/" + TextBox2.Text+"/"+TextBox3.Text));
+    }
+    private void client1_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+    {
+        // check if any error occurred in retrieving service data
+        if (e.Error == null)
+        {
+
+            // parse the returned XML string (e.Result)
+            XDocument xmlResponse = XDocument.Parse(e.Result);
+            TextBox4.Text = xmlResponse.Element(xmlNamespace + "int").Value;
+        }
+    }
+}
